7851906071bfb52a0c03bd0650210d4d46b80468
abeann <63220193+abeann@users.noreply.github.com>
2021-12-31 22:54:52 -0800

    Add files via upload

---
+++ MuteBot/bot.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = '.')
+
+msg = None
+vc  = None
+
+@bot.event
+async def on_ready():
+    print("MuteBot is online!")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def mbping(ctx):
+    global msg
+    msg = await ctx.send('Mute All')
+    await msg.add_reaction('❗')
+    await ctx.message.delete()
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def mbvc(ctx, arg: discord.VoiceChannel):
+    global vc
+    vc = arg
+    await ctx.message.delete()
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    channel = bot.get_channel(payload.channel_id)
+    if (payload.user_id != 776571880194572288 and 
+    payload.message_id == msg.id and 
+    payload.member.guild_permissions.administrator and 
+    payload.emoji.name == '❗'):
+        member_ids = vc.voice_states.keys()
+        for key in member_ids:
+            member = await payload.member.guild.fetch_member(key)
+            await member.edit(mute=True)
+
+@bot.event 
+async def on_raw_reaction_remove(payload):
+    channel = bot.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    if (payload.message_id == msg.id and 
+    message.author.guild_permissions.administrator and
+    payload.emoji.name == '❗'):
+        member_ids = vc.voice_states.keys()
+        for key in member_ids:
+            member = await message.author.guild.fetch_member(key)
+            await member.edit(mute=False)
+
+bot.run('Nzc2NTcxODgwMTk0NTcyMjg4.X6207A.QztjBhdzA1dFSWyKXFPykE1B_zU')
